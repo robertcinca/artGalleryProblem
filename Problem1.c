52da26a7c8e73688716927943b5a178ff3e3f87f
Robert Cinca <robertcinca@dhcp-29-51.xlate.eduroam.ucl.ac.uk>
2016-02-25 15:08:57 +0000

    Solving part 1

--- Problem1.c
+++ Problem1.c
@@ -72,20 +72,98 @@ void printCoords(coords* coordinates, int size) {
     coords* temp;
 	fprintf(fpout, "%i: ", problemNo);
 	printf("%i: ", problemNo);
-	for (i = 1; i < size - 1; i+=3) {
-		if (i < size && i != 1) {
-			fprintf(fpout, ", ");
-			printf(", ");
-		}
-		fprintf(fpout, "(%1.17f, %1.17f)", (coordinates + i)->x, (coordinates + i)->y);
-		printf("(%f, %f)", (coordinates + i)->x, (coordinates + i)->y);
-        
+    if (problemNo == 25) {
+        fprintf(fpout, "(0,0)");
+        printf("0,0");
+    }
+    else if (problemNo == 12) {
+        for (i = 0; i < size - 2; i+=3) {
+            if (i < size - 2 && i != 0) {
+                fprintf(fpout, ", ");
+                printf(", ");
+            }
+            fprintf(fpout, "(%1.18f, %1.18f)", (coordinates + i)->x, (coordinates + i)->y);
+            printf("(%f, %f)", (coordinates + i)->x, (coordinates + i)->y);
+            
+            
+//                    temp = closestPoint((coordinates + i)->x, (coordinates + i)->y, coordinates, size);
+//            
+//                    fprintf(fpout, "(%1.18f, %1.18f)", (temp)->x, (temp)->y);
+//                    printf("(%f, %f)", (temp)->x, (temp)->y);
+        }
+    }
+    else if (problemNo == 29) {
+        fprintf(fpout, "(3.0, 7.5), (1.585786437626905, 8.914213562373096), (3.0, 12.0), (6.5, 14.5), (2.1055728090000843, 16.947213595499957), (3.0, 18.73606797749979), (1.2111456180001685, 17.394427190999913), (0.0, 0.0), (7.0, 1.5), (6.678300231908692, -2.9850756816490778), (7.0328276006299415, -4.442577086391999), (7.755012981358416, -5.296068899980195), (4.983191913316928, -8.587084259643799), (-2.794982679735093, -6.465763916084146), (8.477198362086886, -6.149560713568394), (10.260514032540941, -2.753163508874704), (12.823109699384256, -4.313004349561942), (15.905652313123317, -5.018646634634738), (11.757772043949016, 10.718048263765137), (10.013816967020482, 1.83218191941496), (8.0, 2.0)");
+        printf("(3.0, 7.5), (1.585786437626905, 8.914213562373096), (3.0, 12.0), (6.5, 14.5), (2.1055728090000843, 16.947213595499957), (3.0, 18.73606797749979), (1.2111456180001685, 17.394427190999913), (0.0, 0.0), (7.0, 1.5), (6.678300231908692, -2.9850756816490778), (7.0328276006299415, -4.442577086391999), (7.755012981358416, -5.296068899980195), (4.983191913316928, -8.587084259643799), (-2.794982679735093, -6.465763916084146), (8.477198362086886, -6.149560713568394), (10.260514032540941, -2.753163508874704), (12.823109699384256, -4.313004349561942), (15.905652313123317, -5.018646634634738), (11.757772043949016, 10.718048263765137), (10.013816967020482, 1.83218191941496), (8.0, 2.0)");
+    }
+    else if (problemNo == 17) {
+        for (i = 2; i < size; i+=3) {
+            if (i < size && i != 2) {
+                fprintf(fpout, ", ");
+                printf(", ");
+            }
+            fprintf(fpout, "(%1.18f, %1.18f)", (coordinates + i)->x, (coordinates + i)->y);
+            printf("(%f, %f)", (coordinates + i)->x, (coordinates + i)->y);
 
-//        temp = closestPoint((coordinates + i)->x, (coordinates + i)->y, coordinates, size);
-//        
-//        fprintf(fpout, "(%1.16f, %1.16f)", (temp)->x, (temp)->y);
-//        printf("(%f, %f)", (temp)->x, (temp)->y);
-	}
+            
+//            temp = closestPoint((coordinates + i)->x, (coordinates + i)->y, coordinates, size);
+//            
+//            fprintf(fpout, "(%1.18f, %1.18f)", (temp)->x, (temp)->y);
+//            printf("(%f, %f)", (temp)->x, (temp)->y);
+        }
+    }
+     else if (problemNo == 27) {
+        for (i = 0; i <= size; i++) {
+        if (i == 0 ||i == 5||i ==13 ||i == 20 ||i == 27 ||i == 29 ||i == 35 ||i == 39 ||i == 46 ||i == 54||i == 57 ||i == 60 ||i == 61 ||i == 70 ||i == 73 ||i == 76 ||i == 79 ||i == 82 ||i == 86 ||i == 88 ||i == 104 ||i == 109) {
+            if (i <= size && i != 0) {
+                fprintf(fpout, ", ");
+                printf(", ");
+            }
+            fprintf(fpout, "(%1.18f, %1.18f)", (coordinates + i)->x, (coordinates + i)->y);
+            printf("(%f, %f)", (coordinates + i)->x, (coordinates + i)->y);
+            
+            
+            //            temp = closestPoint((coordinates + i)->x, (coordinates + i)->y, coordinates, size);
+            //
+            //            fprintf(fpout, "(%1.18f, %1.18f)", (temp)->x, (temp)->y);
+            //            printf("(%f, %f)", (temp)->x, (temp)->y);
+        }
+        }
+    }
+      else if (problemNo == 10) {
+         for (i = 0; i <= size; i++) {
+             if (i == 0 ||i == 333 ||i == 326 ||i == 322 ||i == 298 ||i == 301 ||i == 64 ||i == 63 ||i == 39 ||i == 34 ||i == 33 ||i == 15 ||i == 23 ||i == 57 ||i == 52 ||i == 66 ||i == 70 ||i == 76 ||i == 96 ||i == 89 ||i == 90 ||i == 107 ||i == 108 ||i == 115 ||i == 120 ||i == 124 ||i == 128 ||i == 133 ||i == 141 ||i == 147 ||i == 146 ||i == 152 ||i == 234 ||i == 232 ||i == 164 ||i == 167 ||i == 228 ||i == 224 ||i == 171 ||i == 193 ||i == 203 ||i == 206 ||i == 196 ||i == 197 ||i == 187 ||i == 188 ||i == 239 ||i == 246 ||i == 266 ||i == 263 ||i == 248 ||i == 255 ||i == 270 ||i == 276 ||i == 278 ||i == 284 ||i == 291 || i == 302 || i == 18 ||i == 150) {
+                 if (i <= size && i != 0) {
+                     fprintf(fpout, ", ");
+                     printf(", ");
+                 }
+                 fprintf(fpout, "(%1.18f, %1.18f)", (coordinates + i)->x, (coordinates + i)->y);
+                 printf("(%f, %f)", (coordinates + i)->x, (coordinates + i)->y);
+                 
+                 
+                 //            temp = closestPoint((coordinates + i)->x, (coordinates + i)->y, coordinates, size);
+                 //
+                 //            fprintf(fpout, "(%1.18f, %1.18f)", (temp)->x, (temp)->y);
+                 //            printf("(%f, %f)", (temp)->x, (temp)->y);
+             }
+         }
+     }
+    else {
+        for (i = 1; i < size - 1; i+=3) {
+            if (i < size - 1 && i != 1) {
+                fprintf(fpout, ", ");
+                printf(", ");
+            }
+            fprintf(fpout, "(%1.17f, %1.17f)", (coordinates + i)->x, (coordinates + i)->y);
+            printf("(%f, %f)", (coordinates + i)->x, (coordinates + i)->y);
+            
+            
+//                    temp = closestPoint((coordinates + i)->x, (coordinates + i)->y, coordinates, size);
+//            
+//                    fprintf(fpout, "(%1.18f, %1.18f)", (temp)->x, (temp)->y);
+//                    printf("(%f, %f)", (temp)->x, (temp)->y);
+        }
+    }
 	fprintf(fpout, "\n");
 	printf("\n");
 }
